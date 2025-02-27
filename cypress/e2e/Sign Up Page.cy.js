7820a3cbe0cf3c9e7263d4558f0e50680ab05680
mifqiii <mifqiii@gmail.com>
2025-02-27 22:02:01 +0700

    menambahkan TS Sign Up page - Verify User Successfully Creates Account

--- cypress/e2e/Sign Up Page.cy.js
+++ cypress/e2e/Sign Up Page.cy.js
@@ -13,4 +13,17 @@ describe('Sign Up page - Verify User Successfully Creates Account', () => {
     cy.get('button[title="Create an Account"]').click()
     cy.get('.message-success > div').should('be.visible')
   })
+
+  it('Succeeds in creating a new customer account', () => {
+    // Mengisi formulir pendaftaran
+    cy.get('.panel > .header > :nth-child(3) > a').click()
+    cy.get('#firstname').type('Miftahul')
+    cy.get('#lastname').type('Fiqri') 
+    cy.get('#email_address').type('miftahul.fiqri01@sanberdoe.com') //jika ingin melajukan test ubah email terlebih dahulu
+    cy.get('#password').type('Sanber100%') 
+    cy.get('#password-confirmation').type('Sanber100%') 
+    cy.get('button[title="Create an Account"]').click()
+    cy.get('.message-error > div').should('be.visible')
+  })
+
 })
